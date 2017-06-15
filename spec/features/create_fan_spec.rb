ac10750a77a0f278f693d3cf149e5777e5eca76e
Heather Doucet <heatherredoucet@gmail.com>
2017-06-15 12:36:15 -0500

    moving test for create map pin to create_fan spec

--- spec/features/create_fan_spec.rb
+++ spec/features/create_fan_spec.rb
@@ -16,4 +16,20 @@ describe "Creating a new fan" do
     expect(page).to have_text('Thanks for joining the BeauSoleil fanbase!')
     expect(page).to have_text("I just love this band!")
   end
+  it "shows the location pin on map" do
+    visit fans_url
+
+    fill_in "Name", with: "Super Fan"
+    fill_in "Email", with: "fan@super.com"
+    fill_in "City", with: "Portland"
+    select "OR", from: "State"
+    fill_in "Country", with: "USA"
+    fill_in "Message", with: "I just love this band!"
+
+    click_button 'Create Fan'
+
+    expect(page).to have_text('Thanks for joining the BeauSoleil fanbase!')
+    expect(page).to have_text("I just love this band!")
+    expect(page).to have_content("fan marker")
+  end
 end
--- spec/features/create_map_pin_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-describe "showing a fan's location on the map" do
-  it "saves the location and places pin on map" do
-    
-  end
-end
