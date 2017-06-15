199afd0aa54ed4c62ac4f72e574d52bed71e0554
Heather Doucet <heatherredoucet@gmail.com>
2017-06-15 12:26:32 -0500

    adjusted state options to include --

--- app/models/fan.rb
+++ app/models/fan.rb
@@ -2,9 +2,9 @@ class Fan < ApplicationRecord
 
   validates :name, :city, presence: true
 
-  STATES = %w(AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI WY)
+  STATES = %w(AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI WY --)
   validates :state, inclusion: { in: STATES }
 
-  validates :message, length: { maximum: 120 }
+  validates :message, length: { maximum: 150 }
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 end
