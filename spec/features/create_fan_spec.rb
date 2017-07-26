7c869fa9b431d332fac24221340cbe5518826dd3
Heather Doucet <heatherredoucet@gmail.com>
2017-07-26 16:28:39 -0500

    updating rspec tests with new validations

--- spec/features/create_fan_spec.rb
+++ spec/features/create_fan_spec.rb
@@ -8,13 +8,17 @@ describe "Creating a new fan" do
     fill_in "Email", with: "fan@super.com"
     fill_in "City", with: "Portland"
     select "OR", from: "State"
-    fill_in "Country", with: "USA"
+    select "USA", from: "Country"
     fill_in "Message", with: "I just love this band!"
 
     click_button 'Create Fan'
 
     expect(page).to have_text('Thanks for joining the BeauSoleil fanbase!')
     expect(page).to have_text("I just love this band!")
+    expect(page).to have_text("Super Fan")
+    expect(page).to have_text("Portland")
+    expect(page).to have_text("OR")
+    expect(page).to have_text("USA")
   end
   it "shows the location pin on map" do
     visit fans_url
@@ -23,13 +27,11 @@ describe "Creating a new fan" do
     fill_in "Email", with: "fan@super.com"
     fill_in "City", with: "Portland"
     select "OR", from: "State"
-    fill_in "Country", with: "USA"
-    fill_in "Message", with: "I just love this band!"
+    select "USA", from: "Country"
 
     click_button 'Create Fan'
 
-    expect(page).to have_text('Thanks for joining the BeauSoleil fanbase!')
-    expect(page).to have_text("I just love this band!")
-    expect(page).to have_content("fan marker")
+    expect(page).to have_content(@left_marker_url)
+    expect(page).to have_content(@right_marker_url)
   end
 end
--- spec/models/fan_spec.rb
+++ spec/models/fan_spec.rb
@@ -18,7 +18,7 @@ describe "validating a fan" do
     expect(fan.errors[:email].any?).to eq(true)
   end
 
-  it "requires a city" do
+  it "requires a valid city" do
     fan = Fan.new(city: "")
 
     fan.valid?
@@ -33,6 +33,27 @@ describe "validating a fan" do
       fan.valid?
 
       expect(fan.errors[:state].any?).to eq(false)
+    end
+  end
+
+  it "requires a valid country" do 
+    countries = %w[USA Afghanistan Albania Algeria Andorra Angola Antiqua/Barbuda Argentina Armenia Aruba Australia Azerbaijan Bahamas Bahrain Bangladesh Barbados Belarus 
+                 Belgium Belize Benin Bhutan Bolivia Bosnia/Herzegovina Botswana Brazil Brunei Bulgaria BurkinaFaso Burma Burundi Cambodia Cameroon Canada CaboVerde Central/African/Republic 
+                 Chad Chile China Colombia Comoros Congo CostaRica Coted'Ivoire Croatia Cuba Curacao Cyprus Czechia Denmark Djibouti Dominica DominicanRepublic EastTimor Ecuador 
+                 Egypt ElSalvador EquatorialGuinea Eritrea Estonia Ethiopia Fiji Finland France Gabon Gambia Georgia Germany Ghana Greece Grenada Guatemala Guinea Guinea-Bissau
+                 Guyana Haiti Holy-See Honduras HongKong Hungary Iceland India Indonesia Iran Iraq Ireland Israel Italy Jamaica Japan Jordan Kazakhstan Kenya Kiribati Korea-North
+                 Korea-South Kosovo Kuwait Kyrgyzstan Laos Latvia Lebanon Lesotho Liberia Libya Liechtenstein Lithuania Luxembourg Macau Macedonia Madagascar Malawi Malaysia
+                 Maldives Mali Malta MarshallIslands Mauritania Mauritius Mexico Micronesia Moldova Monaco Mongolia Montenegro Morocco Mozambique Namibia Nauru Nepal Netherlands
+                 NewZealand Nicaragua Niger Nigeria Norway Oman Pakistan Palau PalestinianTerritories Panama PapuaNewGuinea Paraguay Peru Philippines Poland Portugal Qatar
+                 Romania Russia Rwanda SaintKitts/Nevis SaintLucia SaintMaarten SaintVincent/Grenadines Samoa SanMarino SaoTome/Principe SaudiArabia Senegal Serbia Seychelles SierraLeone
+                 Singapore Slovakia Slovenia SolomonIslands Somalia SouthAfrica Sudan Spain SriLanka Suriname Swaziland Sweden Switzerland Syria Taiwan Tajikistan Tanzania
+                 Thailand Timor-Leste Togo Tonga Trinidad/Tobago Tunisia Turkey Turkmenistan Tuvalu Uganda Ukraine UnitedArabEmirates UnitedKingdom Uruguay Uzbekistan Vanuatu
+                 Venezuela Vietnam Yemen Zambia Zimbabwe]
+    countries.each do |country|
+      fan = Fan.new(country: country)
+      fan.valid?
+
+      expect(fan.errors[:country].any?).to eq(false)
     end
   end
 end
